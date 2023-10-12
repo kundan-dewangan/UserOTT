c878b1c7255864a34976f5721a7e84a03ee9dfc7
kundan-dewangan <kundan.ev@gmail.com>
2023-10-12 22:32:54 +0530

    All Carousel changes

---
+++ src/components/shared/SectionHeader.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Button, Text, View, StyleSheet} from 'react-native';
+import {sizes, spacing} from '../../constants/theme';
+
+const SectionHeader = ({
+  title,
+  containerStyle,
+  titleStyle,
+  onPress,
+  buttonTitle = 'Button',
+}) => {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Text style={[styles.title, titleStyle]}>{title}</Text>
+      {onPress && <Button title={buttonTitle} onPress={onPress} />}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: spacing.s,
+    marginRight: spacing.m,
+    marginTop: spacing.l,
+  },
+  title: {
+    fontSize: sizes.h2,
+    fontWeight: 'bold',
+    color:'white'
+  },
+});
+
+export default SectionHeader;
--- src/screens/app/HomeScreen.jsx
+++ src/screens/app/HomeScreen.jsx
@@ -5,6 +5,12 @@ import { ScrollView } from 'react-native-gesture-handler'
 import axios from 'axios'
 import { headerPayload } from '../../utils/utils'
 import { useNavigation } from '@react-navigation/native'
+import TopPlacesCarousel from '../../components/Home/TopPlacesCarousel'
+import { PLACES, TOP_PLACES } from '../../data'
+import SectionHeader from '../../components/shared/SectionHeader'
+import MyListCarousel from '../../components/Home/MyListCarousel'
+import TrendingCarousel from '../../components/Home/TrendingCarousel'
+import WatchingForCarousel from '../../components/Home/WatchingForCarousel'
 
 const HomeScreen = () => {
 
@@ -41,14 +47,45 @@ const HomeScreen = () => {
     return (
         <View style={styles.container}>
 
+
             <ScrollView>
+                <SectionHeader
+                    title="Only on OTT"
+                    buttonTitle="See All"
+                    onPress={() => { }}
+                />
+                <TopPlacesCarousel list={TOP_PLACES} />
+
+                <SectionHeader
+                    title="My List"
+                    buttonTitle=""
+                    onPress={false}
+                />
+                <MyListCarousel list={TOP_PLACES} />
+                
+                
+                <SectionHeader
+                    title="Trending on OTT"
+                    buttonTitle=""
+                    onPress={false}
+                />
+                <TrendingCarousel list={TOP_PLACES} />
+
+
+                <SectionHeader
+                    title="Watching for you"
+                    buttonTitle=""
+                    onPress={false}
+                />
+                <WatchingForCarousel list={PLACES} />
+
                 {list?.map((item, index) => {
                     return (<YouTubeCard
                         key={index}
                         title={item?.title}
                         thumbnail={item?.thumbnail}
                         url={item?.url}
-                        onPress={()=> handleClick(item)} />)
+                        onPress={() => handleClick(item)} />)
                 })}
             </ScrollView>
         </View>
