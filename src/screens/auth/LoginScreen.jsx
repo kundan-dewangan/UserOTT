4c7ebb7971b879b10e6f0767c7ff7d929a2ece4f
kundan-dewangan <kundan.ev@gmail.com>
2023-10-12 20:20:49 +0530

    Login Changes

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -52,7 +52,7 @@ const LoginScreen = () => {
         <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
             <Formik
-                initialValues={{ email: 'mohan@gmail.com', password: 'Mohan@1234' }}
+                initialValues={{ email: '', password: '' }}
                 validationSchema={validationSchema}
                 onSubmit={handleLogin}
             >
